7971db0b4635ea14defc3c31f56d4091b856dfe5
Safayat Zisan <zisan_cse_mbstu@hotmail.com>
2016-11-14 13:16:46 +0600

    Add project files.

---
+++ MVC with bootstrap/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_with_bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<string> aList = new List<string>()
+            {
+                "Dhaka",
+                "Tangail",
+                "Pabna"
+            };
+            ViewBag.Cities = aList;
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC with bootstrap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_with_bootstrap.MvcApplication" Language="C#" %>
